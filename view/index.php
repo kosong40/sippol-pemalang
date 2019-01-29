52f7f3de60c7764c8b842274fb481aa37c6d3ae1
cahyo40 <mdcnugroho@gmail.com>
2019-01-29 20:58:24 +0700

    cek

--- view/index.php
+++ view/index.php
@@ -99,17 +99,21 @@
     </div>
     <div class="conteiner"><h4 align="center"><b>----- Layanan -----</b></h4></div>
     <div class="row">
-        <div class="col-sm-3">
+    <div class="col-sm-3">
             <img src="../aset/img/no_img.svg" class="img-rounded img-responsive" alt="">
+            <h4 align="center">Izin Mendirikan Bangunan</h4>
         </div>
         <div class="col-sm-3">
             <img src="../aset/img/no_img.svg" class="img-rounded img-responsive" alt="">    
+            <h4 align="center">Izin Reklame</h4>
         </div>
         <div class="col-sm-3">
             <img src="../aset/img/no_img.svg" class="img-rounded img-responsive" alt="">    
+            <h4 align="center">Izin Usaha Mikro dan Kecil</h4>
         </div>
         <div class="col-sm-3">
             <img src="../aset/img/no_img.svg" class="img-rounded img-responsive" alt="">    
+            <h4 align="center">Izin Pariwisata</h4>
         </div>
     </div>
   </div>
---
+++ view/surat/iumk.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="shortcut icon" type="image/x-icon" href="">
+	<title>SURAT IZIN USAHA MIKRO DAN KECIL</title>
+	<!-- bootstrap css -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<!-- bootstrap js -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<style>
+	@media print {
+  /* style sheet for print goes here */
+  .noprint {
+    visibility: hidden;
+  }
+}
+</style>
+<body>
+	
+	<div class="container">
+	<div style="margin: 10px;">
+		<button class="noprint btn btn-primary" onclick="window.print()"><span class="glyphicon glyphicon-print"></span> Cetak</button>
+	</div>
+	</div>
+	<div class="container" >
+		<div class="header-form">
+			<div class="row">
+				
+				<h3 align="center" style="margin-top: 250px"><b>PEMERINTAH KABUPATEN PEMALANG</b></h3>
+				<h3 align="center" style="margin-top: -23px"><b>KECAMATAN PEMALANG</b></h3>
+				
+				<h4 align="center"><b>SURAT IZIN USAHA MIKRO DAN KECIL</b></h4>
+				<h3 align="center" style="margin-top: -25px"><b>Nomor :</b></h3>
+				<p align="justify" style="margin-right: 10%; margin-left: 10%">Berdasarkan Peraturan Presiden Nomor 98 Tahun 2014 tentang Perizinan untuk Usaha Mikro dan Kecil (Lembar Negara Republik Indonesia Tahun 2014 Nomor 222); Peraturan Menteri Dalam Negeri Republik Indonesia Nomor 83 Tahun 2014 tentang Pedoman Pemberian Izin Usaha Mikro dan Kecil;</p>
+			</div>
+			<div class="row" style="margin-left: 10%; margin-right: 10%">
+				<table>
+					<tr>
+						<td width="200" >Serta</td>
+						
+					</tr>
+					<tr>
+						<td >Nomor Perbub Pemalang</td>
+						<td>: 33 TAHUN 2015</td>
+					</tr>
+					
+					<tr>
+						<td align="top">Tentang</td>
+						<td>: Perubahan Atas Peraturan Bupati Pemalang Nomor 49 Tahun 2012 Tentang Pelimpahan Sebagian Kewenangan Bupati Kepada Camat</td>
+					</tr>
+					
+				</table>
+				<p style="margin-bottom: -1px">Bersama ini menyatakan dan memberikan izin kepada : </p>
+				<table>
+					<tr>
+						<td width="200">Nama</td>
+						<td>: 33 TAHUN 2015</td>
+					</tr>
+					
+					<tr>
+						<td width="200" align="top">Nomor KTP</td>
+						<td>: 331021200xxx</td>
+					</tr>
+					<tr>
+						<td width="200" align="top">Nomor Telpon</td>
+						<td>: 331021200xxx</td>
+					</tr>
+				</table>
+				<p align="justify" style="margin-bottom: -1px">Untuk mendirikan Usaha Mikro dan Kecil yang mencakup perizinan dasar yang berupa : menempati lokasi / domisili, melakukan kegiatan usaha baik produksi maupun penjualan barang dan jasa, dengan identitas :</p>
+				<table >
+					<tr height="5" style="margin-bottom: : -2px" >
+						<td width="200" align="top">Nama Usaha / Nama Toko</td>
+						<td>: 331021200xxx</td>
+					</tr>
+					<tr>
+						<td width="200">Alamat</td>
+						
+					</tr>
+					
+					<tr>
+						<td width="200" align="top">Provinsi</td>
+						<td>: 331021200xxx</td>
+					</tr>
+					<tr>
+						<td width="200" align="top">Kabupaten</td>
+						<td>: 331021200xxx</td>
+					</tr>
+					<tr>
+						<td width="200" align="top">Kecamatan</td>
+						<td>: 331021200xxx</td>
+					</tr>
+					<tr>
+						<td width="200" align="top">Desa</td>
+						<td>: 331021200xxx</td>
+					</tr>
+					<tr>
+						<td width="200" align="top">Nama Jalan / Dusun</td>
+						<td>: 331021200xxx</td>
+					</tr>
+					<tr>
+						<td width="200" align="top">Kode Pos</td>
+						<td>: 331021200xxx</td>
+					</tr>
+					<tr>
+						<td width="200" align="top">Sektor Usaha</td>
+						<td>: 331021200xxx</td>
+					</tr>
+					<tr>
+						<td width="200" align="top">Sarana yang digunakan</td>
+						<td>: 331021200xxx</td>
+					</tr>
+					<tr>
+						<td width="200" align="top">Jumlah Modal Usaha</td>
+						<td>: 331021200xxx</td>
+					</tr>
+					<tr>
+						<td width="200" align="top">NPWP</td>
+						<td>: 331021200xxx</td>
+					</tr>
+					<tr>
+						<td width="200" align="top">Klasifikasi Usaha</td>
+						<td>: 331021200xxx</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+		
+	<br><br>
+		<div class="footer-form">
+			<div class="row" style="margin: 2% 2%">
+				<table class="table">
+					<tr>
+						<td rowspan="6" width="500" style="border: none;">xxxx</td>
+						<td  style="border: none;">Ditetapkan di</td>
+						<td style="border: none;">: Pemalang</td>
+					</tr>
+					<tr>
+						<td style="border: none;">Pada tanggal</td>
+						<td  style="border: none;">: asasas</td>
+						
+					</tr>
+					<tr>
+						<td colspan="2" style="border: none;">CAMAT PEMALANG</td>
+						
+				</tr>
+					<tr>			
+						<td colspan="2" style=" border: none;"><br><br><br><br><b>SUHIRMAN, S.Sos.M.Si</b></td>
+					</tr>
+					<tr>			
+						<td colspan="2" style="border: none;">Pembina Tingkat I</td>
+					</tr>
+					<tr>			
+						<td colspan="2" style=" border: none;"><b>NIP. 19671213 199803 1 005</b></td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</div>
+
+	
+	<style>
+		.body-form th{
+			text-align: center;
+			font-size: 15px;
+			line-height: 70%;
+			padding: 3px !important;
+		}
+		.body-form td{
+			text-align: center;
+			font-size: 15px;
+			line-height: 70%;
+			padding: 3px !important;
+		}
+		#tabel td, #tabel th{
+			border: 1px solid black;
+		}
+		.footer-form td{
+			text-align: center;
+			font-size: 17px;
+		}
+		#bank td, #bank th{
+			text-align: left;
+			font-size: 14px;
+			border: 1px solid black;
+		}
+		#header td{
+			font-size: 18px;
+			line-height: 110%;
+		}
+
+	</style>
+</body>
+</html>
